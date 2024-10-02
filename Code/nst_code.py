c0db2d1e9f111810cd7f1fdda12d469baeebd84a
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-10-03 00:04:51 +0530

    Added image processing code

---
+++ Code/nst_code.py
@@ -0,0 +1,72 @@
+import os
+import tensorflow as tf
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+import IPython.display as display
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+import PIL.Image
+import time
+import functools
+import cv2
+
+
+def tensor_to_image(tensor):
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    
+    # Convert to BGR format for OpenCV (if needed)
+    if tensor.shape[-1] == 3:  # RGB image
+        tensor = cv2.cvtColor(tensor, cv2.COLOR_RGB2BGR)
+    
+    # Display image using OpenCV
+    cv2.imshow('Generated Image', tensor)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    # Save image
+    cv2.imwrite('Data/generated_image.png', tensor)
+
+content_path = r'/home/om/Desktop/Nest-Neural-Style-Transfer/aaa_1.jpg'
+style_path = r'/home/om/Desktop/Nest-Neural-Style-Transfer/aaa_2.jpg'
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def imshow(image, title=None):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
+
+  plt.imshow(image)
+  if title:
+    plt.title(title)
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+import tensorflow_hub as hub
+hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+tensor_to_image(stylized_image)
